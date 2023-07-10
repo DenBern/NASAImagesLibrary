9382d340b7a1b37c0b1590b1438eb92f212be1d6
Bernovich Denis <denisbernovich@gmail.com>
2023-07-10 22:12:41 +0300

    fix: remove datepicker, add inputs number

--- src/components/DateFilter/DateFilter.jsx
+++ src/components/DateFilter/DateFilter.jsx
@@ -1,28 +1,25 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../../context/Context.jsx";
-import { Button } from "@mui/joy";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Button, Input } from "@mui/joy";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SearchIcon from "@mui/icons-material/Search";
-import { DesktopDatePicker } from "@mui/x-date-pickers";
 import { useNASAService } from "../../service/NASAService.js";
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 
 import "./DateFilter.scss";
 
 export const DateFilter = () => {
-  const [startYearValue, setStartYearValue] = useState();
-  const [endYearValue, setEndYearValue] = useState();
-
-  const {startYear, currentYear} = useNASAService();
+  const { startYear, currentYear } = useNASAService();
   const {
-    yearStart,
-    yearEnd,
     setYearStart,
     setYearEnd,
     setPage,
     setSearchParams,
   } = useContext(Context);
+
+  const [startYearValue, setStartYearValue] = useState('');
+  const [endYearValue, setEndYearValue] = useState('');
+
 
   const activeFilter = () => {
     setYearStart(startYearValue);
@@ -36,13 +33,9 @@ export const DateFilter = () => {
     setYearEnd(currentYear);
     setSearchParams(`page=${1}`);
     setPage(1);
+    setStartYearValue('');
+    setEndYearValue('');
   }
-
-  // useEffect(() => {
-  //   console.log('effect')
-  //   setStartYearValue('');
-  //   setYearEnd('');
-  // }, [yearStart, yearEnd])
 
   return (
     <div className="date-filter">
@@ -53,26 +46,58 @@ export const DateFilter = () => {
         <DeleteIcon/>
       </Button>
       <div
+        className="date-inputs"
         style={
           {
             display: "flex",
             gap: "10px"
           }
         }
-        className="date-pickers"
       >
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DesktopDatePicker
-          label="Year start"
-          views={['year']}
-          onChange={(value) => setStartYearValue(value.$y)}
+        <Input
+          value={startYearValue}
+          onChange={(e) => setStartYearValue(e.target.value)}
+          variant="outlined"
+          size="sm"
+          placeholder="Year start"
+          type="number"
+          sx={
+            {
+              '--Input-focusedInset': 'var(--any, )',
+              '--Input-focusedThickness': '0.25rem',
+              '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
+              '&::before': {
+                transition: 'box-shadow .15s ease-in-out',
+              },
+              '&:focus-within': {
+                borderColor: '#86b7fe',
+              },
+            }
+          }
+          endDecorator={<CalendarMonthOutlinedIcon/>}
         />
-          <DesktopDatePicker
-            label={"Year end"}
-            views={['year']}
-            onChange={(value) => setEndYearValue(value.$y)}
-          />
-        </LocalizationProvider>
+        <Input
+          value={endYearValue}
+          onChange={(e) => setEndYearValue(e.target.value)}
+          variant="outlined"
+          size="sm"
+          placeholder="Year end"
+          type="number"
+          sx={
+            {
+              '--Input-focusedInset': 'var(--any, )',
+              '--Input-focusedThickness': '0.25rem',
+              '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
+              '&::before': {
+                transition: 'box-shadow .15s ease-in-out',
+              },
+              '&:focus-within': {
+                borderColor: '#86b7fe',
+              },
+            }
+          }
+          endDecorator={<CalendarMonthOutlinedIcon/>}
+        />
       </div>
       <Button
         className="btn-filter-search"
