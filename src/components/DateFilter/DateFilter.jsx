ca80140cebf64e6b3b752a330efcb4fef3fa227c
Bernovich Denis <denisbernovich@gmail.com>
2023-07-11 21:25:43 +0300

    fix: return date picker component

--- src/components/DateFilter/DateFilter.jsx
+++ src/components/DateFilter/DateFilter.jsx
@@ -1,10 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNASAService } from "../../service/NASAService.js";
 import { Context } from "../../context/Context.jsx";
-import { Button, Input } from "@mui/joy";
+import { Button } from "@mui/joy";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SearchIcon from "@mui/icons-material/Search";
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from 'dayjs';
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 import "./DateFilter.scss";
 
@@ -17,33 +20,29 @@ export const DateFilter = () => {
     setSearchParams,
   } = useContext(Context);
 
-  const [startYearValue, setStartYearValue] = useState('');
-  const [endYearValue, setEndYearValue] = useState('');
-
-  const [startYearError, setStartYearError] = useState("");
-  const [endYearError, setEndYearError] = useState("");
+  const [startYearValue, setStartYearValue] = useState(null);
+  const [endYearValue, setEndYearValue] = useState(null);
 
   const activeFilter = () => {
-    setStartYearError("");
-    setEndYearError("");
-
-    if(startYearValue  === '' && endYearValue === '') {
+    if(startYearValue  === null && endYearValue === null) {
       setYearStart(startYear);
       setYearEnd(currentYear);
-    } else if (endYearValue === '') {
+    } else if (endYearValue === null) {
       setYearStart(startYearValue);
       setYearEnd(currentYear);
+    } else if (startYearValue === null) {
+      setYearStart(startYear);
+      setYearEnd(endYearValue);
     } else {
       setYearStart(startYearValue);
       setYearEnd(endYearValue);
     }
+
     setSearchParams(`page=${1}`);
     setPage(1);
   }
 
   const clearFilter = () => {
-    setStartYearError("");
-    setEndYearError("");
     setYearStart(startYear);
     setYearEnd(currentYear);
     setSearchParams(`page=${1}`);
@@ -52,105 +51,36 @@ export const DateFilter = () => {
     setEndYearValue('');
   }
 
-const handleStartYearChange = (e) => {
-  const value = e.target.value;
-  setStartYearValue(value);
-
-  if (value < 1 || value > currentYear) {
-    setStartYearError("error");
-    setStartYearValue(startYearError);
-  } else {
-    setStartYearError("");
-  }
-};
-
-const handleEndYearChange = (e) => {
-  const value = e.target.value;
-  setEndYearValue(value);
-
-  if (value < 1 || value > currentYear) {
-    setEndYearError("error");
-    setEndYearValue(endYearError);
-  } else {
-    setEndYearError("");
-  }
-};
-
   return (
     <div className="date-filter">
       <Button
         className="btn-filter-clear"
-        onClick={() => clearFilter()}
+        onClick={clearFilter}
       >
         <DeleteIcon/>
       </Button>
-      <div
-        className="date-inputs"
-        style={
-          {
-            display: "flex",
-            gap: "10px"
-          }
-        }
-      >
-        <Input
-          helpertext={startYearError}
-          error={startYearError !== ""}
-          min="1"
-          max={currentYear}
-          maxLength={4}
-          value={startYearValue}
-          onChange={handleStartYearChange}
-          variant="outlined"
-          size="sm"
-          placeholder="Year start"
-          type="number"
-          sx={
-            {
-              '--Input-focusedInset': 'var(--any, )',
-              '--Input-focusedThickness': '0.25rem',
-              '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
-              '&::before': {
-                transition: 'box-shadow .15s ease-in-out',
-              },
-              '&:focus-within': {
-                borderColor: '#86b7fe',
-              },
-            }
-          }
-          endDecorator={<CalendarMonthOutlinedIcon/>}
-        />
-        <Input
-          helpertext={endYearError}
-          error={endYearError !== ""}
-          min="1"
-          max={currentYear}
-          maxLength={4}
-          value={endYearValue}
-          onChange={handleEndYearChange}
-          variant="outlined"
-          size="sm"
-          placeholder="Year end"
-          type="number"
-          sx={
-            {
-              '--Input-focusedInset': 'var(--any, )',
-              '--Input-focusedThickness': '0.25rem',
-              '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
-              '&::before': {
-                transition: 'box-shadow .15s ease-in-out',
-              },
-              '&:focus-within': {
-                borderColor: '#86b7fe',
-              },
-            }
-          }
-          endDecorator={<CalendarMonthOutlinedIcon/>}
-        />
+      <div className="date-pickers">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DesktopDatePicker
+            yearsPerRow={4}
+            minDate={dayjs('0001')}
+            maxDate={dayjs(`${endYearValue ?? currentYear}`)}
+            label={"Year start"}
+            views={['year']}
+            onChange={(value) => setStartYearValue(value.$y)}
+          />
+          <DesktopDatePicker
+            minDate={dayjs(`${startYearValue ?? startYear}`)}
+            maxDate={dayjs(`${currentYear}`)}
+            label={"Year end"}
+            views={['year']}
+            onChange={(value) => setEndYearValue(value.$y)}
+          />
+        </LocalizationProvider>
       </div>
       <Button
         className="btn-filter-search"
-        onClick={() => activeFilter()}
+        onClick={activeFilter}
       >
         <SearchIcon/>
       </Button>
