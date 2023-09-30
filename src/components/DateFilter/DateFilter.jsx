f329eb5d8d6808f35e64f9a769dea45d35b7ae21
Denis <denisbernovich@gmail.com>
2023-09-30 20:09:40 +0300

    fix: delete png

--- src/components/DateFilter/DateFilter.jsx
+++ src/components/DateFilter/DateFilter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNASAService } from "../../service/NASAService.js";
 import { Context } from "../../context/Context.jsx";
 import { Button } from "@mui/joy";
